da5d5a7f3f066362fca6a337ac4f59ec9c7fe103
Kedar Agate <kedar.agate@gmail.com>
2023-12-30 23:23:47 +0530

    building food delivery app

--- Food Delivery App/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const AppLayout = <div>Hello World</div>;
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(AppLayout);
